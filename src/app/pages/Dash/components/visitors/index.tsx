a0dce83a4ae912512ddfad5f43bcb4d7f62b9efb
Henrique da Silva Alves <henriquehdsa@gmail.com>
2020-05-29 17:22:51 -0300

    reajuste no cards

--- src/app/pages/Dash/components/visitors/index.tsx
+++ src/app/pages/Dash/components/visitors/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './visitors.scss';
 
 
 const Visitors: React.FC = () => {
     return (
-        <section className="card">
+        <section className="card card-visitors">
             <div className="card-header">
                 <h3>Visitors by Browser</h3>
                 <div> --Selector-- </div>
--- src/app/pages/Dash/index.tsx
+++ src/app/pages/Dash/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import Visitors from './components/visitors';
+import './dash.scss';
 
 const Dash: React.FC = () => {
     return (
-        <h1>
-            <span>Home</span>
-        </h1>
+        <section className="page-dash">
+            <div className="wrap-1">
+                <div className="box">
+                   <Visitors />
+                </div>
+                <div className="box"> --Widgets-- </div>
+            </div>
+        </section>
     );
-}
+};
 
 export default Dash;
